315c4e056c3f37570097e3eb779e4e36023b38b6
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-09-01 12:57:14 +0530

    Backend Setup for calling agent

---
+++ backend/src/index.ts
@@ -0,0 +1,47 @@
+import express, { Application, Request, Response } from "express";
+import axios from "axios";
+import cors from "cors";
+import { fetchRawTextByFname } from "./util/farcaster";
+import dotenv from "dotenv";
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Example route to make an API call
+app.get("/api/data", async (req: Request, res: Response) => {
+  try {
+    // You can define a type for response data if known
+    const response = await axios.get("https://api.example.com/data");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log("worked");
+  console.log(process.env.PRIVATE_KEY);
+  res.json("Worked");
+});
+
+app.get("/api/social/farcaster", async (req: Request, res: Response) => {
+  try {
+    const fname = req.query.fname as string;
+    const response = await fetchRawTextByFname(fname);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
